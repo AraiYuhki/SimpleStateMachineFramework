090413554e6706281be50dd040e303f79eb9b2a5
xeon_lagunas <xeon.lagunas@gmail.com>
2025-02-04 10:15:47 +0900

    待機ステート追加

---
+++ Packages/SimpleStateMachine/Runtime/Scripts/WaitState.cs
@@ -0,0 +1,17 @@
+namespace Xeon.StateMachine
+{
+    public class WaitState : IState
+    {
+        public void OnEnter(object bridgeParam)
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
